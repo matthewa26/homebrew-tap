cebc879179db23b403f3cf1492615ddf67efa7c6
Matthew Ayers <matthew@ayers.sh>
2026-05-04 10:36:31 -0400

    pr-scout 0.1.1
    Bumps url + sha256 to v0.1.1 and updates the test assertion to match
    the new --version output. Upstream release notes:
    https://github.com/matthewa26/pr-scout/releases/tag/v0.1.1

--- Formula/pr-scout.rb
+++ Formula/pr-scout.rb
@@ -1,8 +1,8 @@
 class PrScout < Formula
   desc "Scan GitHub PRs across local repos and surface ones that need attention"
   homepage "https://github.com/matthewa26/pr-scout"
-  url "https://github.com/matthewa26/pr-scout/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "c33d6d3c0445d88c1dddf094773675ea8806cace1d18d812b8ef1a6e109942e8"
+  url "https://github.com/matthewa26/pr-scout/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "4b4d644851ab2e68c38f10e9e4c82c1e3e45b6621865bb0bf922b35326fcc7ee"
   license "Apache-2.0"
   head "https://github.com/matthewa26/pr-scout.git", branch: "main"
 
@@ -16,6 +16,6 @@ class PrScout < Formula
   end
 
   test do
-    assert_match "0.1.0", shell_output("#{bin}/pr-scout --version")
+    assert_match "0.1.1", shell_output("#{bin}/pr-scout --version")
   end
 end
